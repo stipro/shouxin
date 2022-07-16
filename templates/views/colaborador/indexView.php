8f27828557969aebbb1f2f13d75fae5c78c6c3ef
Laptop Asus <stipro150197@gmail.com>
2022-07-15 20:03:25 -0500

    Diseño

---
+++ templates/views/colaborador/indexView.php
@@ -0,0 +1,287 @@
+<?php require_once INCLUDES . 'inc_header.php'; ?>
+<div class="container">
+  <div class="card">
+    <div class="card-header d-flex justify-content-between">
+      <h3 class="card-title">Colaborador</h3>
+    </div>
+    <div class="card-body">
+      <h5>Por el presente documento declaro bajo juramento lo siguiente:</h5>
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+              <ol type="I" start="1">
+                <li>Que los datos y demás información consignada en el presente documento son verdaderos y actuales, obligándome frente a MSP S.A. a presentarla anualmente, con los datos actualizados a la fecha de presentación, en concordnacia con la Resolución SBS N° 789-2018. Asimismo, autorizo a la empresa la verificación de la presente información.
+              </ol>
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+            <div class="accordion-body">
+              <div class="text-success">
+                <hr>
+                <h3>DATOS PERSONALES</h3>
+                <hr>
+              </div>
+
+              <div class="row">
+                <div class="col-12 col-sm-4">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Apellido Paterno<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-4">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Apellido Materno<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-sm-4">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Nombres<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-3">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Nacionalidad<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12 col-sm-3">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Dep. de Nacimiento<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-3">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Provincia de Nacimiento<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-3">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Distrito de Nacimiento<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Fecha de Nacimiento<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="date" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-6">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Documento de Indentificación<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 col-sm-6">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Numero de Indentificación<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Profesión, Arte u Oficio<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Ocupación/ Cargo en la Empresa<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Estado Civil<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Condición del inmueble en el que vive<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">Registro SUNAT<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="form-group">
+                    <label for="insertIpt-number-income" class="control-label">INFORMACION COMPLEMENTARIA<span class="text-danger">*</span></label>
+                    <div class="input-group input-group-sm">
+                      <input type="text" name="number-income" id="insertIpt-number-income" class="form-control form-control-sm disabled" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="text-success">
+                  <hr>
+                  <h3>ESTUDIOS REALIZADOS</h3>
+                  <hr>
+                </div>
+                <!-- Section: Timeline -->
+                <section class="">
+
+                  <ul class="timeline">
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">[Nivel de estudio]</h5>
+                      <p class="text-muted mb-2 fw-bold">[Centro educativo]</p>
+                      <p class="text-muted mb-2 fw-bold">[F. Inicio - F. Conclusión]</p>
+                    </li>
+
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">First customer</h5>
+                      <p class="text-muted mb-2 fw-bold">19 March 2020</p>
+                      <p class="text-muted">
+                        Quisque ornare dui nibh, sagittis egestas nisi luctus nec. Sed
+                        aliquet laoreet sapien, eget pulvinar lectus maximus vel.
+                        Phasellus suscipit porta mattis.
+                      </p>
+                    </li>
+
+                  </ul>
+                </section>
+                <!-- Section: Timeline -->
+                <div class="text-success">
+                  <hr>
+                  <h3>EXPERIENCIA LABORAL</h3>
+                  <hr>
+                </div>
+
+                <!-- Section: Timeline -->
+                <section class="">
+
+                  <ul class="timeline">
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">[Cargo]</h5>
+                      <p class="text-muted mb-2 fw-bold">[Empresa]</p>
+                      <p class="text-muted mb-2 fw-bold">[F. Inicio - F. Conclusión]</p>
+                    </li>
+
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">First customer</h5>
+                      <p class="text-muted mb-2 fw-bold">19 March 2020</p>
+                      <p class="text-muted">
+                        Quisque ornare dui nibh, sagittis egestas nisi luctus nec. Sed
+                        aliquet laoreet sapien, eget pulvinar lectus maximus vel.
+                        Phasellus suscipit porta mattis.
+                      </p>
+                    </li>
+
+                  </ul>
+                </section>
+                <!-- Section: Timeline -->
+
+                <div class="text-success">
+                  <hr>
+                  <h3>CAPACITACIÓN</h3>
+                  <hr>
+                </div>
+
+                <!-- Section: Timeline -->
+                <section class="">
+
+                  <ul class="timeline">
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">[Curso o Evento]</h5>
+                      <p class="text-muted mb-2 fw-bold">[institución o organizad@]</p>
+                      <p class="text-muted mb-2 fw-bold">[F. Curso o Evento]</p>
+                    </li>
+
+                    <li class="timeline-item mb-5">
+                      <h5 class="fw-bold">First customer</h5>
+                      <p class="text-muted mb-2 fw-bold">19 March 2020</p>
+                      <p class="text-muted">
+                        Quisque ornare dui nibh, sagittis egestas nisi luctus nec. Sed
+                        aliquet laoreet sapien, eget pulvinar lectus maximus vel.
+                        Phasellus suscipit porta mattis.
+                      </p>
+                    </li>
+
+                  </ul>
+                </section>
+                <!-- Section: Timeline -->
+                <div class="text-success">
+                  <hr>
+                  <h3>INFORMACIÓN PATRIMONIAL</h3>
+                  <hr>
+                </div>
+              </div>
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+              <ol type="I" start="2">
+                <li>No tener deudas vencidas mayores a ciento veinte (120) días ni créditos castigados.
+              </ol>
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+              <ol type="I" start="3">
+                <li> No registrar información en el Registro de Deudores Aliementarios Morosos (REDAM), ni en el Registro de Deudores de Reparaciones Civiles (REDERECI).
+              </ol>
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once INCLUDES . 'inc_footer.php'; ?>
